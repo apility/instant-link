71f3a01f7d85f457033e1268dfc6656f0c4aa03d
Thomas Alrek <thomas@apility.no>
2016-11-27 23:30:18 +0100

    Removed prototypes, added jsdoc

--- src/cache.js
+++ src/cache.js
@@ -1,42 +1,60 @@
+/**
+ * Exports the Cache class
+ * @module cache
+ */
+
 const lzma = require('lzma-purejs');
 
+/**
+ * Creates a new Cache instance
+ * @param {boolean} compress - Enable/disable compression
+ */
 class Cache{
     constructor(compress){
         this.compress = compress || false;
         this.data = {}
     }
-}
 
-Cache.prototype.store = function(key, val){
-    if(typeof this.data[key] === 'undefined'){
-        try{
-            if(this.compress){
-                let data = new Buffer(val, 'utf8');
-                this.data[key] = lzma.compressFile(data);
-            }else{
-                this.data[key] = val;
+    /**
+     * Store a key in the cache
+     * @param {string} key
+     * @param {string} val
+     */
+    store(key, val){
+        if(typeof this.data[key] === 'undefined'){
+            try{
+                if(this.compress){
+                    let data = new Buffer(val, 'utf8');
+                    this.data[key] = lzma.compressFile(data);
+                }else{
+                    this.data[key] = val;
+                }
+            }catch(e){
+                return;
             }
-        }catch(e){
-            return;
         }
     }
-}
 
-Cache.prototype.retrieve = function(key){
-    if(typeof this.data[key] !== 'undefined'){
-        try{
-            if(this.compress){
-                let data = lzma.decompressFile(this.data[key]);
-                return new Buffer(data).toString('utf8');
+    /**
+     * Retrieves a key from the cache
+     * @param {string} key
+     */
+    retrieve(key){
+        if(typeof this.data[key] !== 'undefined'){
+            try{
+                if(this.compress){
+                    let data = lzma.decompressFile(this.data[key]);
+                    return new Buffer(data).toString('utf8');
 
-            }else{
-                return this.data[key];
+                }else{
+                    return this.data[key];
+                }
+            }catch(e){
+                return undefined;
             }
-        }catch(e){
-            return undefined;
         }
+        return undefined;
     }
-    return undefined;
 }
 
 export default Cache;
